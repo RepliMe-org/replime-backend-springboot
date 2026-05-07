0871e2c154d4e5d48502ea7567cb2d83bf6eb084
Salmanahmed22 <123300970+Salmanahmed22@users.noreply.github.com>
2026-05-07 14:59:40 +0300

    add ChatSession and Message entities services and controller

--- src/main/java/com/example/demo/services/MessageService.java
+++ src/main/java/com/example/demo/services/MessageService.java
@@ -1,9 +1,14 @@
 package com.example.demo.services;
 
+import com.example.demo.repos.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MessageService {
+
+    @Autowired
+    private MessageRepo messageRepo;
 
     // Mock implementation for existsByMessageClassId
     public boolean existsByMessageClassId(Long messageClassId) {
